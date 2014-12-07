9ab3fcdbc86e1d9d61422c5ea753016d4954b523
Brian Surowiec <xtorted@optonline.net>
2014-12-07 12:25:02 +0500

    Add message builder unit test

---
+++ test/Raygun.Owin.Tests/Tests/RaygunMessageBuilderTests.cs
@@ -0,0 +1,35 @@
+﻿namespace Raygun.Tests
+{
+    using System;
+
+    using NUnit.Framework;
+
+    using Raygun.Messages;
+
+    using Shouldly;
+
+    [TestFixture]
+    public class RaygunMessageBuilderTests
+    {
+        [Test]
+        public void Should_set_error_for_exception()
+        {
+            // Given / When
+            var sut = SutFactory();
+            var result = sut.Details.Error;
+
+            // Then
+            result.ClassName.ShouldBe("System.InvalidOperationException"); 
+            result.Message.ShouldBe("InvalidOperationException: It's broken");
+        }
+
+        private RaygunMessage SutFactory()
+        {
+            var exception = new InvalidOperationException("It's broken");
+
+            return RaygunMessageBuilder.New
+                                       .SetExceptionDetails(exception)
+                                       .Build();
+        }
+    }
+}
