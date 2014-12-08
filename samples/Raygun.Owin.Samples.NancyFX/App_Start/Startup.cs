1a1241905a72710873b8a158aa2c2e8eeb535214
Brian Surowiec <xtorted@optonline.net>
2014-12-09 04:55:08 +0500

    Add sample using nancy

---
+++ samples/Raygun.Owin.Samples.NancyFX/App_Start/Startup.cs
@@ -0,0 +1,65 @@
+﻿[assembly: Microsoft.Owin.OwinStartup(typeof(Raygun.Owin.Samples.NancyFX.Startup))]
+
+namespace Raygun.Owin.Samples.NancyFX
+{
+    using System;
+
+    using global::Owin;
+
+    using Microsoft.Owin;
+
+    using Nancy;
+    using Nancy.Extensions;
+    using Nancy.Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var settings = new RaygunSettings();
+
+#if DEBUG
+            settings.Tags.Add("debug");
+#endif
+
+            app.UseRaygun(options =>
+            {
+                options.LogUnhandledExceptions = true;
+                options.LogUnhandledRequests = true;
+                options.Settings = settings;
+            });
+
+            app.Use((context, next) =>
+            {
+                if (context.Request.Path.StartsWithSegments(new PathString("/exception")))
+                {
+                    throw new NotImplementedException("Route: /exception");
+                }
+
+                return next();
+            });
+
+            app.UseNancy(options =>
+            {
+                options.PerformPassThrough = context =>
+                {
+                    if (context.Response.StatusCode == HttpStatusCode.InternalServerError)
+                    {
+                        Exception exception;
+                        if (context.TryGetException(out exception))
+                        {
+                            if (exception is RequestExecutionException)
+                            {
+                                exception = exception.InnerException;
+                            }
+
+                            new RaygunClient(settings).SendInBackground(context.GetOwinEnvironment(), exception);
+                        }
+                    }
+
+                    return false;
+                };
+            });
+        }
+    }
+}
---
+++ samples/Raygun.Owin.Samples.NancyFX/Modules/HomeModule.cs
@@ -0,0 +1,38 @@
+﻿namespace Raygun.Owin.Samples.NancyFX.Modules
+{
+    using System;
+
+    using Nancy;
+    using Nancy.Cookies;
+
+    public class HomeModule : NancyModule
+    {
+        private const string CookieName = "raygun.owin.sample";
+
+        public HomeModule()
+        {
+            Before.AddItemToStartOfPipeline(ctx =>
+            {
+                if (!ctx.Request.Cookies.ContainsKey(CookieName))
+                {
+                    ctx.NegotiationContext.Cookies.Add(new NancyCookie(CookieName, DateTime.UtcNow.ToString("U"), true, false, null));
+                }
+
+                return null;
+            });
+
+            Get["/"] = _ => View["Index"];
+
+            Get["/error"] = _ =>
+            {
+                throw new NotImplementedException("Route: /error");
+            };
+
+            Get["/form-error"] = _ => View["FormError"];
+            Post["/form-error"] = _ =>
+            {
+                throw new NotImplementedException("Route: /form-error");
+            };
+        }
+    }
+}
