720f51e604a3fd4a71118adf71277ca08fef229e
Brian Surowiec <xtorted@optonline.net>
2014-12-12 05:38:45 +0500

    Add base startup class with raygun error handling

---
+++ src/Raygun.Owin.AppBuilder/RaygunStartup.cs
@@ -0,0 +1,69 @@
+﻿namespace Raygun.Owin
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using global::Owin;
+
+    using OwinEnvironment = System.Collections.Generic.IDictionary<string, object>;
+
+    public abstract class RaygunStartup
+    {
+        protected virtual string ApplicationVersion()
+        {
+            return null;
+        }
+
+        protected virtual RaygunSettings SetupSettings()
+        {
+            return new RaygunSettings
+            {
+                MessageInspector = message =>
+                {
+                    var version = ApplicationVersion();
+                    if (version != null)
+                    {
+                        message.Details.Version = version;
+                    }
+                }
+            };
+        }
+
+        public virtual void Configuration(IAppBuilder app)
+        {
+            var settings = SetupSettings();
+
+            try
+            {
+                SetupErrorHandling(settings);
+                SetupConfiguration(app, settings);
+            }
+            catch (Exception ex)
+            {
+                ReportError(settings, ex);
+
+                throw;
+            }
+        }
+
+        public abstract void SetupConfiguration(IAppBuilder app, RaygunSettings settings);
+
+        public virtual void SetupErrorHandling(RaygunSettings settings)
+        {
+            TaskScheduler.UnobservedTaskException += (sender, args) =>
+            {
+                ReportError(settings, args.Exception);
+            };
+        }
+
+        public void ReportError(RaygunSettings settings, Exception exception)
+        {
+            ReportError(settings, null, exception);
+        }
+
+        public virtual void ReportError(RaygunSettings settings, OwinEnvironment environment, Exception exception)
+        {
+            new RaygunClient(settings).SendInBackground(environment, exception);
+        }
+    }
+}
