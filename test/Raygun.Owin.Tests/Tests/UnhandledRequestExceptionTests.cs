ce7626a2f7a383eee37323af844fbbbefbde94bd
Brian Surowiec <xtorted@optonline.net>
2016-05-28 14:32:07 -0400

    Include url in the default message
    Closes #21

---
+++ test/Raygun.Owin.Tests/Tests/UnhandledRequestExceptionTests.cs
@@ -0,0 +1,58 @@
+﻿namespace Raygun.Tests
+{
+    using System;
+
+    using NUnit.Framework;
+
+    using Shouldly;
+
+    [TestFixture]
+    public class UnhandledRequestExceptionTests
+    {
+        [Test]
+        public void Should_set_url()
+        {
+            // Given
+            var url = "/some/page";
+
+            // When
+            var result = new UnhandledRequestException(url);
+
+            // Then
+            result.RequestUrl.ShouldBe(url);
+            result.Message.ShouldBe("The request was unhandled: /some/page");
+        }
+
+        [Test]
+        public void Should_set_url_and_message()
+        {
+            // Given
+            var url = "/some/page";
+            var message = "This is a test.";
+
+            // When
+            var result = new UnhandledRequestException(url, message);
+
+            // Then
+            result.RequestUrl.ShouldBe(url);
+            result.Message.ShouldBe(message);
+        }
+
+        [Test]
+        public void Should_set_url_message_and_innerexception()
+        {
+            // Given
+            var url = "/some/page";
+            var message = "This is a test.";
+            var innerException = new InvalidOperationException();
+
+            // When
+            var result = new UnhandledRequestException(url, message, innerException);
+
+            // Then
+            result.RequestUrl.ShouldBe(url);
+            result.Message.ShouldBe(message);
+            result.InnerException.ShouldBe(innerException);
+        }
+    }
+}
